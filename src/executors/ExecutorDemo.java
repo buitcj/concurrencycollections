90d87619edbab743599862b59b9169c2fca3fb33
buitcj <julianbui@gmail.com>
2017-05-12 00:23:42 +0700

    Add example for executor and callable

---
+++ src/executors/ExecutorDemo.java
@@ -0,0 +1,56 @@
+package executors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorDemo {
+
+	public static class PrimerFinderTask implements Callable<String> {
+		
+		
+		private int num;
+		
+		public PrimerFinderTask(int num) {
+			this.num = num;
+		}
+
+		@Override
+		public String call() throws Exception {
+			
+			for (int i = 2; i < num; i++) {
+				if (num % i == 0) {
+					return num + " was divisible by : " + i;
+				}
+			}
+			
+			return "Prime found: " + num;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ExecutorService es = Executors.newFixedThreadPool(10);
+		
+		List<Future<String>> futures = new ArrayList<Future<String>>();
+		for (int i = 0; i < 10000000; i++) {
+			Future<String> future = es.submit(new PrimerFinderTask(i));
+			futures.add(future);
+		}
+		
+		futures.forEach(f -> {
+			try {
+				System.out.println(f.get());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+	}
+}
